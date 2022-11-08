09f7765905271030767d0bef92de1908ad63d710
spectrumstrike <108103800+spectrumstrike@users.noreply.github.com>
2022-11-08 13:50:39 -0500

    Add files via upload

---
+++ Hello World!.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+//always include iostream as it allows stuff to run
+int main() {
+    std::cout << "Hello World!" << endl;
+    /*standard allows use of cout which outputs the text in quotations
+    endl ends the line*/
+}
